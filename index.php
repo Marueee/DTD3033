86b20bce0974f0ba6b02fc08816003cac665a6a2
Amirul Azim Bin Apandi <d20221101850@siswa.upsi.edu.my>
2025-01-07 20:44:14 +0800

    add background image

--- index.php
+++ index.php
@@ -8,12 +8,32 @@
 
     <!-- Favicon -->
     <link rel="icon" href="img/Icon.png" type="image/png">
-    
+
     <link rel="stylesheet" href="css/styles.css">
 </head>
 
-<body>
+<body class="home-page">
+
     <?php include 'navigation.php'; ?>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            min-height: 100vh;
+            position: relative;
+
+            /* Add background image */
+            background-image: url('img/hotel_bg.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-color: rgba(0, 0, 0, 0.1); 
+            z-index: 0;
+        }
+    </style>
 
     <div class="content">
         <h1>Welcome to Our Hotel</h1>
