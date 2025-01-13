2c1749caef881bd03e401184c000aafafdaa399f
Marueee <d20221101806@siswa.upsi.edu.my>
2025-01-13 17:28:24 +0800

    s

--- navbar.php
+++ navbar.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	<div class="container">
 		<a class="navbar-brand" href="index.php">La<span>Passion</span></a>
